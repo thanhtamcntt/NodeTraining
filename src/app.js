b7bb6fa741778e9bb59e45bb9aaba8713f2dd64c
Hoang Loc <hoangphuocloc.phurieng@gmail.com>
2023-10-30 21:07:24 +0700

    setup code #3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-console.log(process.env.port)
-const PORT = process.env.port ? process.env.port : 4000;
+console.log(process.env.PORT)
+const PORT = process.env.PORT ? process.env.PORT : 4000;
 
 
 app.listen(PORT, () => {
